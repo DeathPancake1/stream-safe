fa9d1ccdab85440ac24887b63a1d51dfde0ea977
Youssef Dawoud <youssefyoyododo@gmail.com>
2024-02-02 23:10:57 +0200

    Changed message appearence

--- renderer/components/Message.tsx
+++ renderer/components/Message.tsx
@@ -1,5 +1,8 @@
-import { Box } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { Video } from "../indexedDB";
+import DownloadIcon from '@mui/icons-material/Download';
+import { useUser } from "../providers/UserContext";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 interface Props {
     incoming: boolean;
@@ -7,14 +10,25 @@ interface Props {
 }
 
 export default function Message({ incoming, message }: Props) {
+  const {userData, updateUser} = useUser()
   const backgroundColor = incoming ? '#e0e0e0' : '#333333'; // Adjust colors as needed
   const textColor = incoming ? '#333' : '#fff'; // Adjust text color for better contrast
+
+  const download = async ()=>{
+
+  }
+
+  const playVideo = async()=>{
+
+  }
 
   return (
     <Box
       sx={{
-        width: '40%',
-        height: '100px',
+        width: 'fit-content',
+        minWidth: '10%',
+        maxWidth: '40%',
+        height: 'fit-content',
         backgroundColor: backgroundColor,
         borderRadius: '8px',
         boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
@@ -24,17 +38,46 @@ export default function Message({ incoming, message }: Props) {
         fontWeight: 'bold',
         marginTop: '10px',
         display: "flex",
+        marginRight: "20px",
         flexDirection: 'column'
       }}
     >
-      <Box>
-        {message.name}
+      <Box
+        sx={{
+          margin: '2px',
+          textAlign: 'center',
+          overflowX: 'hidden'
+        }}
+      >
+        <Typography fontFamily={'roboto'} fontSize={24} >
+          {message.name.slice(0, -4)}
+        </Typography>
       </Box>
       <Box>
-        Test
-      </Box>
-      <Box>
-        Test
+        {
+          message.downloaded?
+          <Button 
+            component="label" 
+            color="secondary"
+            variant="text" 
+            startIcon={<PlayArrowIcon />} 
+            onClick={()=>console.log('not implemented')} 
+            fullWidth
+          >
+            Play Video
+          </Button>
+          :
+          <Button
+            component="label" 
+            variant="text" 
+            startIcon={<DownloadIcon />} 
+            onClick={()=>download()} 
+            fullWidth
+          >
+            Download file
+          </Button>
+        }
+        
       </Box>
     </Box>
   );
