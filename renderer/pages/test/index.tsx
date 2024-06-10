ca656cc9c48ba05923ac26782360cbca96dcd548
amgedelmasry <amgedelmasry76@gmail.com>
2024-06-10 23:41:40 +0300

    fixed sidebar along _app and channelInfo is now page

--- renderer/pages/test/index.tsx
+++ renderer/pages/test/index.tsx
@@ -2,10 +2,17 @@ import { ThemeProvider } from "@emotion/react";
 import theme from "../../themes/theme";
 import { Box } from "@mui/material";
 import SideBar from "../../components/sideBar/sideBar";
-import ChannelInfo from "../../components/channelInfo/channelInfo";
+import ChannelInfo from "../channelInfo/[id]";
 import AllChannelPage from "../../components/allChannelPage/allChannelPage";
+import { useRouter } from "next/router";
 
 export default function Test(){
+  const router = useRouter();
+
+  const goToChannelInfo = () => {
+      const channelId = "bf275cd7-5c57-49fd-a166-11b4e6c75093";
+      router.push(`/channelInfo/${channelId}`);
+  };
     return (
         <ThemeProvider theme={theme}>
         <Box
@@ -16,8 +23,9 @@ export default function Test(){
             marginTop: '5%'
           }}
         >
-          <SideBar childrenFunction={<AllChannelPage/>}/>
-          {/*ChannelInfo({id:"d61e7a63-9363-4eae-b7e1-2619722889db"})*/}
+          <button onClick={goToChannelInfo}>Go to Channel Info</button>
+
+          {/* ChannelInfo({id:"d61e7a63-9363-4eae-b7e1-2619722889db"}) */}
           
         </Box>
       </ThemeProvider>
