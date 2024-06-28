a98979a6b8d5ed62509db315d9735a25214be037
Youssef Dawoud <youssefyoyododo@gmail.com>
2024-06-29 02:29:04 +0300

    Changed channel card dimensions

--- renderer/components/channelCard/channelCard.tsx
+++ renderer/components/channelCard/channelCard.tsx
@@ -1,18 +1,14 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import {
-    useGetPhotoByPath,
-    useGetPhotoPathById,
-} from "../../api/hooks/photo-hook";
+import { useGetPhotoByPath, useGetPhotoPathById } from "../../api/hooks/photo-hook";
 import { useEffect, useState } from "react";
 import { useUser } from "../../providers/UserContext";
 import { useRouter } from "next/router";
 import { useUserById } from "../../api/hooks/user-by-id-hook";
+import { CardContent, CardMedia } from "@mui/material";
 
 interface User {
     firstname: string;
@@ -20,25 +16,13 @@ interface User {
     photoId: number | null;
 }
 
-export default function ChannelCard({
-    title,
-    imageId,
-    description,
-    ownerId,
-    channelId,
-}) {
+export default function ChannelCard({ title, imageId, description, ownerId, channelId }) {
     const { userData, updateUser } = useUser();
-    const { mutate: getPhotoPathById, isLoading: getPhotoPathByIdLoading } =
-        useGetPhotoPathById();
-    const { mutate: getPhotoByPath, isLoading: getPhotoByPathLoading } =
-        useGetPhotoByPath();
+    const { mutate: getPhotoPathById, isLoading: getPhotoPathByIdLoading } = useGetPhotoPathById();
+    const { mutate: getPhotoByPath, isLoading: getPhotoByPathLoading } = useGetPhotoByPath();
     const [photoPath, setPhotoPath] = useState<string>("");
     const [imageUrl, setImageUrl] = useState<string>("");
-    const [owner, setOwner] = useState<User>({
-        firstname: "",
-        lastname: "",
-        photoId: 0,
-    });
+    const [owner, setOwner] = useState<User>({ firstname: "", lastname: "", photoId: 0 });
     const { mutate: getUserById } = useUserById();
     const router = useRouter();
 
@@ -47,63 +31,59 @@ export default function ChannelCard({
     };
 
     useEffect(() => {
-        getUserById(
-            { ownerId: ownerId, jwt: userData.jwt },
-            {
-                onSuccess: (response) => {
-                    if (response.status === 201) {
-                        setOwner(response.data.message);
-                    }
-                },
-            }
-        );
+        getUserById({ ownerId: ownerId, jwt: userData.jwt }, {
+            onSuccess: (response) => {
+                if (response.status === 201) {
+                    setOwner(response.data.message);
+                }
+            },
+        });
     }, []);
 
     useEffect(() => {
         if (imageId) {
-            getPhotoPathById(
-                { photoId: imageId, jwt: userData.jwt },
-                {
-                    onSuccess: (response) => {
-                        if (response.status === 201) {
-                            var apiPath = response.data.message;
-                            setPhotoPath(apiPath);
-                        }
-                    },
-                }
-            );
+            getPhotoPathById({ photoId: imageId, jwt: userData.jwt }, {
+                onSuccess: (response) => {
+                    if (response.status === 201) {
+                        var apiPath = response.data.message;
+                        setPhotoPath(apiPath);
+                    }
+                },
+            });
         }
     }, [imageId]);
+
     useEffect(() => {
         if (photoPath) {
-            getPhotoByPath(
-                { photoPath, jwt: userData.jwt },
-                {
-                    onSuccess: async (response) => {
-                        if (response.status === 201) {
-                            const url = await URL.createObjectURL(
-                                response.data
-                            );
-                            setImageUrl(url);
-                        } else {
-                            setImageUrl(
-                                "https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg"
-                            );
-                        }
-                    },
-                }
-            );
+            getPhotoByPath({ photoPath, jwt: userData.jwt }, {
+                onSuccess: async (response) => {
+                    if (response.status === 201) {
+                        const url = await URL.createObjectURL(response.data);
+                        setImageUrl(url);
+                    } else {
+                        setImageUrl("https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg");
+                    }
+                },
+            });
         } else {
-            setImageUrl(
-                "https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg"
-            );
+            setImageUrl("https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg");
         }
     }, [photoPath]);
+
     return (
-        <Card sx={{ maxWidth: 345, minWidth: 250, height: "330px" }}>
+        <Card sx={{ maxWidth: 345, minWidth: 250, height: "300px", display: "flex", flexDirection: "column" }}>
             <CardMedia sx={{ height: 140 }} image={imageUrl} title={title} />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
+            <CardContent sx={{ flexGrow: 1 }}>
+                <Typography 
+                    gutterBottom 
+                    variant="h5" 
+                    component="div" 
+                    sx={{ 
+                        whiteSpace: "nowrap", 
+                        overflow: "hidden", 
+                        textOverflow: "ellipsis"
+                    }}
+                >
                     {title}
                 </Typography>
                 <Typography sx={{ mb: 1.5 }} color="text.secondary">
@@ -112,7 +92,11 @@ export default function ChannelCard({
                 <Typography
                     variant="body2"
                     color="text.secondary"
-                    style={{ overflow: "hidden" }}
+                    sx={{
+                        whiteSpace: "nowrap",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis"
+                    }}
                 >
                     {description}
                 </Typography>
