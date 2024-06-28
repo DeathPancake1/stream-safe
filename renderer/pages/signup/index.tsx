a8cf66284bede16bfc1a8af8aa00d95536db54d0
Youssef Dawoud <youssefyoyododo@gmail.com>
2024-06-29 01:49:00 +0300

    Fixed signup page

--- renderer/pages/signup/index.tsx
+++ renderer/pages/signup/index.tsx
@@ -13,8 +13,6 @@ export default function Page() {
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
-          width: '100%',
-          height: '100%',
         }}
       >
         <Welcome />
